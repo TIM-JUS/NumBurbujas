97e076ca09ac20d633e3dc8cd53e0aefbf2777a8
TIM-JUS <1440406@senati.pe>
2023-08-17 19:26:30 -0500

    Entrada de datos

--- OrdenNum/src/main/java/com/mycompany/ordennum/OrdenNum.java
+++ OrdenNum/src/main/java/com/mycompany/ordennum/OrdenNum.java
@@ -16,25 +16,6 @@ public class OrdenNum {
         System.out.println("==BIENVENIDO A MI PROGRAMA \nNUMEROS BURBUJAS===");
         Scanner scanner =new Scanner(System.in);
         int[] arreglo = new int[5];
-        
-        for (int i = 0; i < arreglo.length; i++) {
-            System.out.print("INGRESE EL NUMERO " + (i + 1) + ": ");
-            arreglo[i] = scanner.nextInt();
-        }
-    scanner.close();
-        System.out.print("\nLOS NUMEROS SON: ");
-        for (int num : arreglo) System.out.print(num + " ");
-        System.out.println();
-        
-        for (int i = 0; i < arreglo.length - 1; i++)
-            for (int j = 0; j < arreglo.length - i - 1; j++)
-                if (arreglo[j] > arreglo[j + 1]) {
-                    int temp = arreglo[j];
-                    arreglo[j] = arreglo[j + 1];
-                    arreglo[j + 1] = temp;
-                }
-        System.out.print("\nLOS NUEVOS NUMEOS SON: ");
-        for (int num : arreglo) System.out.print(num + " ");
-        System.out.println();
+
     }
 }
